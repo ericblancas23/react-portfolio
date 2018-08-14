7cd4ef3b110946afcd42a25eb3f43db9b3842e87
EricB <ericblancas23@gmail.com>
2018-08-14 13:29:05 -0700

    added resume and new things

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 
+
 export const NavBar = () => (
   <div className="nav">
       <nav className="navbar navbar-light bg-light navbar-expand-md nav">
-      <a href="/" className="navbar-brand" style={{ color: "white" }}>Eric Blancas</a>
-       <a href="https://www.linkedin.com/in/eric-blancas23/"><img alt="" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-256.png" style={{width: 30, height: 30, float: "right", marginRight: "1cm"}}/></a>
-       <a href="https://github.com/ericblancas23"><img alt="" src={require("../img/Octocat.png")} style={{width: 50, height: 50, float: "right"}}/></a>
+      <a href="/" className="navbar-brand" style={{ color: "black" }}>Eric Blancas</a>
+      <a className="navbar-brand" style={{color: "white"}} href="https://drive.google.com/file/d/1ngzxPG9Kawhp-_g7nSXKjvMB5vxQdw9K/view?ths=true" style={{ marginLeft: "1cm"}}>Resume</a>
+       <a href="https://www.linkedin.com/in/eric-blancas23/"><img alt="" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-256.png" style={{width: 30, height: 30, marginLeft: "1cm"}}/></a>
+       <a href="https://github.com/ericblancas23"><img alt="" src={require("../img/Octocat.png")} style={{width: 50, height: 50, marginLeft:"1.5cm"}}/></a>
       </nav>
   </div>
 );
