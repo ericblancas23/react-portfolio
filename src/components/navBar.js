5f29dfe33f0c06f0c96849cd42baa8ce85933b5e
EricB <ericblancas23@gmail.com>
2018-08-20 14:50:52 -0700

    added blog link

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -7,8 +7,10 @@ export const NavBar = () => (
       <nav className="navbar navbar-light bg-light navbar-expand-md nav">
       <a href="/" className="navbar-brand" style={{ color: "black" }}>Eric Blancas</a>
       <a className="navbar-brand" style={{color: "white"}} href="https://drive.google.com/file/d/1ngzxPG9Kawhp-_g7nSXKjvMB5vxQdw9K/view?ths=true" style={{ marginLeft: "1cm"}}>Resume</a>
+      <a className="navbar-brand" style={{color: "white"}} href="https://ericb404.blogspot.com/" style={{ marginLeft: "1cm"}}>Blog</a>
        <a href="https://www.linkedin.com/in/eric-blancas23/"><img alt="" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-256.png" style={{width: 30, height: 30, marginLeft: "1cm"}}/></a>
        <a href="https://github.com/ericblancas23"><img alt="" src={require("../img/Octocat.png")} style={{width: 50, height: 50, marginLeft:"1.5cm"}}/></a>
+       
       </nav>
   </div>
 );
