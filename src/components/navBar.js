d902a0d39f1eb1b9a0625f8e60c8b0376f902dc2
EricB <ericblancas23@gmail.com>
2019-03-14 14:42:33 -0700

    responsive styling

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -6,8 +6,8 @@ export const NavBar = () => (
   <div className="nav">
       <nav className="navbar navbar-light bg-light navbar-expand-md nav">
       <a href="/" className="navbar-brand" style={{ color: "black" }}>Eric Blancas</a>
-      <a className="navbar-brand" style={{color: "white"}} href="https://drive.google.com/file/d/1Bqz235cMe_mvBMRI-ce-V17FKdyW5EqN/view?usp=sharing" style={{ marginLeft: "1cm"}}>Resume</a>
-      <a className="navbar-brand" style={{color: "white"}} href="https://ericb404.blogspot.com/" style={{ marginLeft: "1cm"}}>Blog</a>
+      <a className="navbar-brand" style={{color: "white"}} href="https://drive.google.com/file/d/1RaRA20tU7lv-sYvD44dJ4l7JWIi4pD8r/view?usp=sharing" style={{ marginLeft: "1cm"}}>Resume</a>
+      {/* <a className="navbar-brand" style={{color: "white"}} href="https://ericb404.blogspot.com/" style={{ marginLeft: "1cm"}}>Blog</a> */}
       </nav>
   </div>
 );
